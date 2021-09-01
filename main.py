a2340b57817be9c29bc34e80dd0c1ef5eb7942ea
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2021-09-01 22:09:18 +0000

    Moved all badwords into enviroment secrets.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from discord.ext import commands
-from dotenv import load
+# import dotenv
 import os
 
-load()
+# dotenv.load()
 
 # Load environment secrets
 TOKEN = os.getenv('TOKEN')
 PREFIX = os.getenv('PREFIX')
+BADWORDS = os.getenv('BADWORDS')
 
-with open('badwords') as f:
-    badWords = f.read()
-
+BADWORDS = BADWORDS.split(',')
 
 bot = commands.Bot(command_prefix=PREFIX)
 
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    else:
+        await message.channel.send(message.content)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! Bot Latency: {round (bot.latency * 1000)} ms')
 
-
-print("Bot Is Running!")
 
 bot.run(TOKEN)
