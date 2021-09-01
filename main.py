59e23412ab6ca59c6283857d288254618e34dd33
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2021-09-01 23:19:39 +0000

    Deletes badwords (then dm's the user about it). Also added a purge command which is functional

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
 from discord.ext import commands
-# import dotenv
 import os
 
-# dotenv.load()
 
 # Load environment secrets
 TOKEN = os.getenv('TOKEN')
 PREFIX = os.getenv('PREFIX')
-BADWORDS = os.getenv('BADWORDS')
+BADWORDS = os.getenv('BADWORDS').split(',')
+hello = ['Hi', 'hi', 'Yo', 'yo','HI', 'Hello']
 
-BADWORDS = BADWORDS.split(',')
 
-bot = commands.Bot(command_prefix=PREFIX)
+bot = commands.Bot(command_prefix='?')
 
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Bot Latency: {round (bot.latency * 1000)} ms')
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def purge(ctx, amount=0):
+    await ctx.channel.purge(limit=amount+1)
+   
+
+@bot.event
+async def on_member_join(member):
+    print(f' {member} has joined the server')
+  
 
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
 
+
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    else:
-        await message.channel.send(message.content)
+    print(str(message.author) + ": " +message.content)
+    words = message.content.split(' ')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! Bot Latency: {round (bot.latency * 1000)} ms')
+
+    for word in words:
+        if word in hello:
+            await message.add_reaction('👋') 
+
+        if word in BADWORDS:
+            await message.author.send(f'Hey <@{str(message.author.id)}>! Swearing in the server is not allowed! Please do not continue with this behavior or else you may get banned!')
+            
+            await message.delete()
+
+    await bot.process_commands(message)
 
 
 bot.run(TOKEN)
